85e7675890648614dea9c116d97691afa7e1f49b
aslammardin <aslam99mardin@gmail.com>
2023-07-09 18:34:56 +0700

    fix benerin typedjs

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,6 @@
 const multiText = document.querySelector('.multiText')
 
-var typed = new Typed(multiText, {
+var typed = new Typed(".multiText", {
     strings: ['Desainer', 'Programer', 'Nerwork', 'Hardware', 'Office'],
     typeSpeed: 200,
 });
