2d881679ec44a1dcbe485d0afc1e54053f9400a0
aslammardin <aslam99mardin@gmail.com>
2023-09-13 19:11:18 +0700

    coba push

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,2 @@
 const multiText = document.querySelector('.multiText')
 
-var typed = new Typed(".multiText", {
-    strings: ['Desainer', 'Programer', 'Nerwork', 'Hardware', 'Office'],
-    typeSpeed: 200,
-});
